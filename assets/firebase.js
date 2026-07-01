57ede3d7c284809db0a6e97fc60bb08f188913fa
Peiiyong <jemins2001@gmail.com>
2026-07-01 16:21:59 +0800

    install firebase

---
+++ assets/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAIHqtd1KHzKjd8MP45JJqxauyzz8IbJxc",
+  authDomain: "reactnativeapp-5cbb4.firebaseapp.com",
+  projectId: "reactnativeapp-5cbb4",
+  storageBucket: "reactnativeapp-5cbb4.firebasestorage.app",
+  messagingSenderId: "1016632280855",
+  appId: "1:1016632280855:web:ad99092b96d90f0c309952",
+  measurementId: "G-KNJP0J2GLP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
