03bbf30d1cd886e6a54402f3d1a99b662d16aa11
Peiiyong <jemins2001@gmail.com>
2026-07-09 15:03:43 +0800

    + scroll to right icon in gameCarousel

--- src/components/GameCarousel.tsx
+++ src/components/GameCarousel.tsx
@@ -1,4 +1,6 @@
+import { Ionicons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
+import { useRef } from "react";
 import { Dimensions, FlatList, Image, Pressable, StyleSheet, Text, View } from "react-native";
 import { useThemeColors } from "../theme/useThemeColors";
 
@@ -21,6 +23,7 @@ export default function GameCarousel({
   onMorePress,
 }:GameCarouselProps){
   const colors = useThemeColors();
+  const listRef = useRef<FlatList>(null);
   const {width} = Dimensions.get("window");
   // screen show 2 cards
   const cardWidth = (width - 60) / 2;
@@ -34,62 +37,90 @@ export default function GameCarousel({
     }
   ];
 
-  return (
-    <FlatList
-      data={displayGames}
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      keyExtractor={(item)=>item.id}
-      contentContainerStyle={{ gap:12 }}
-      renderItem={({item})=>{
-        const isMore = item.id==="more";
+  const scrollNext = () => {
+    listRef.current?.scrollToOffset({
+      offset: cardWidth + 12,
+      animated:true
+    });
+  };
 
-        if(isMore){
-          return (
-            <Pressable  onPress={onMorePress} style={{ width: cardWidth }}>
-              <LinearGradient colors={colors.cardBackground} style={styles.card}>
-                <Image
-                source={require("../../assets/images/moreGame.png")}
-                style={styles.image}
-                resizeMode="cover"
-                />
-                <View style={styles.info}>
+  return (
+    <View>
+      <FlatList
+        ref={listRef}
+        data={displayGames}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item)=>item.id}
+        contentContainerStyle={{
+          gap:12,
+          paddingRight:50
+        }}
+        renderItem={({item})=>{
+          const isMore = item.id==="more";
+
+          if(isMore){
+            return (
+              <Pressable  onPress={onMorePress} style={{ width: cardWidth }}>
+                <LinearGradient colors={colors.cardBackground} style={styles.card}>
+                  <Image
+                  source={require("../../assets/images/moreGame.png")}
+                  style={styles.image}
+                  resizeMode="cover"
+                  />
+                  <View style={styles.info}>
                     <Text style={[ styles.gameName,{ color:colors.text }]}>🎮 More Games</Text>
                     <Text style={[ styles.level,{ color: colors.navDefaultIcon }]}>Explore more games</Text>
+                  </View>
+                </LinearGradient>
+              </Pressable>
+            );
+          }
+
+          return (
+            <Pressable onPress={()=>onGamePress(item)} style={{ width: cardWidth }}>
+              <LinearGradient colors={colors.cardBackground} style={styles.card}>
+              {
+                item.gamePicture ? (
+                  <Image source={{  uri:item.gamePicture}}
+                    style={styles.image}
+                    resizeMode="cover"
+                  />
+                ):(
+                  <View style={[ styles.imagePlaceholder, { backgroundColor: colors.navDefaultIcon}]}/>
+                )}
+
+                <View style={styles.info}>
+                  <Text numberOfLines={1} style={[ styles.gameName,{ color:colors.text}]}>
+                    {item.gameName}
+                  </Text>
+
+                  <Text style={[ styles.level,{ color: colors.navDefaultIcon }]}>
+                    Lv.{item.requiredLevel ?? 1}
+                  </Text>
                 </View>
               </LinearGradient>
             </Pressable>
           );
-        }
+        }}
+      />
 
-        return (
-          <Pressable onPress={()=>onGamePress(item)} style={{ width: cardWidth }}>
-            <LinearGradient colors={colors.cardBackground} style={styles.card}>
-            {
-              item.gamePicture ? (
-                <Image source={{  uri:item.gamePicture}}
-                  style={styles.image}
-                  resizeMode="cover"
-                />
-              ):(
-                <View style={[ styles.imagePlaceholder, { backgroundColor: colors.navDefaultIcon}]}/>
-            )}
-
-            <View style={styles.info}>
-              <Text numberOfLines={1} style={[ styles.gameName,{ color:colors.text}]}>
-                {item.gameName}
-              </Text>
-
-              <Text style={[ styles.level,{ color: colors.navDefaultIcon }]}>
-                Lv.{item.requiredLevel ?? 1}
-              </Text>
-            </View>
-            </LinearGradient>
+      { displayGames.length > 2 &&
+        (
+          <Pressable
+            onPress={scrollNext}
+            style={[ styles.nextButton, { backgroundColor:colors.primary}]}
+          >
+            <Ionicons
+              name="chevron-forward"
+              size={22}
+              color={colors.text2}
+            />
           </Pressable>
-        );
-      }}
-    />
-  );
+        )
+      }
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -99,11 +130,13 @@ const styles = StyleSheet.create({
     overflow:"hidden",
   },
 
+
   image:{
     width:"100%",
     height:100,
     borderRadius:16,
   },
+
 
   imagePlaceholder:{
     width:"100%",
@@ -111,10 +144,12 @@ const styles = StyleSheet.create({
     borderRadius:16,
   },
 
+
   info:{
     marginTop:10,
     gap:4,
   },
+
 
   gameName:{
     fontSize:15,
@@ -122,15 +157,22 @@ const styles = StyleSheet.create({
     fontFamily:"Baloo2",
   },
 
+
   level:{
     fontSize:12,
     fontFamily:"Baloo2",
   },
 
-  moreContainer:{
-    height:100,
-    borderRadius:16,
-    justifyContent:"center",
+
+  nextButton:{
+    position:"absolute",
+    right:4,
+    top:"40%",
+    width:35,
+    height:35,
+    borderRadius:20,
     alignItems:"center",
+    justifyContent:"center",
+    elevation:5,
   },
 });
